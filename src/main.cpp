ed65730fc92a2e2c14d99b0286fae40469d63961
Andres Aleman <andresaleman142@gmail.com>
2024-08-02 17:51:26 -0600

    Botón auxiliar implementado

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,53 @@ Finite State Machine (FSM) to implement control with a specific time and duratio
 #define INT_PIN 15
 #define CAN_ID 0x01
 #define CTRL_PIN 5
-#define PIN_RX2 16
-#define PIN_TX2 17
+#define BUTTON_PIN 12
+#define RX2_PIN 16
+#define TX2_PIN 17
 #define MODBUS_MODE_PIN 13
+
+#if eRobExp
+  //Actuator Variables
+  float position;
+  float degPosition;
+  float velocity;
+  float degVelocity;
+  float torque;
+  float minTorque = -7.0;  // Minimum allowable torque
+  float maxTorque = 7.0;  // Maximum allowable torque
+  float setpointTorque = 5.0;
+  float dynamicTorqueSensor;
+
+  //Time Variables
+  const long duration = 30; //in Sec
+  const long frecuency = 10; //in Hz
+  const long printingFrecuency = 100; //in ms
+  unsigned long currentMillis;
+  unsigned long previousMillisDuration;
+  unsigned long previousMillisFrecuency;
+  unsigned long previousMillisPrintingFrecuency;
+  bool flag = false;
+
+  // GPIO pin to configure the interrupt
+  volatile bool has_interrupted = false;
+
+  // Return to Zero Mode
+  float posError;
+  float derivativeError;
+  float previousError = 0; // Store the previous error
+  float targetZero = 0.0;
+  float Kp = 15.0; // Proportional gain, adjust as needed
+  float Kd = 10.0; // Derivative gain, adjust as needed
+  float toleranceZero = 0.5; // Define a small tolerance for zero position
+  unsigned long currentZeroMillis;
+  unsigned long previousZeroMillis = 0;
+  const long intervalZero = 50; // Check every 50 milliseconds
+#endif
 
 #if DynTest //Usar el dinamometro externo
   // data array for modbus network sharing
   uint16_t au16data[16];    //Max Lenght Buffer Array
-  SoftwareSerial mySerial(PIN_RX2, PIN_TX2); //Create a SoftwareSerial object so that we can use software serial. Search "software serial" on Arduino.cc to find out more details.
+  SoftwareSerial mySerial(RX2_PIN, TX2_PIN); //Create a SoftwareSerial object so that we can use software serial. Search "software serial" on Arduino.cc to find out more details.
   Modbus master(0, mySerial, MODBUS_MODE_PIN);   //Last parameter 
   modbus_t telegram;
   float dynamometer = 0;
@@ -41,14 +80,14 @@ Finite State Machine (FSM) to implement control with a specific time and duratio
 
 #if rampTest //Ramp variables
 float startRampValue = 0.0; // Initial value of the ramp
-float finalRampValue = 15.0; // Final peak value
+float finalRampValue = 10.0; // Final peak value
 float incrementRamp = 0.1; // Increment step size
 float decrementRamp = 0.0; // Decrement step size
 float currentRampValue; // Current value of the ramp
 bool isRamping = false; // State of the ramp function
 bool rampCompleted = false; // Flag to indicate if ramp is completed
 unsigned long previousRampMillis = 0; // Store the last time the ramp value was updated
-const unsigned long intervalRamp = 150; // Interval between updates in milliseconds
+const unsigned long intervalRamp = 100; // Interval between updates in milliseconds
 
 // Function to start the ramp
 void startRamp() {
@@ -91,194 +130,177 @@ void runRamp() {
     }
   }
 }
-
 #endif
 
 #if eRobExp
-
-  //Actuator Variables
-  float position;
-  float degPosition;
-  float velocity;
-  float degVelocity;
-  float torque;
-  float minTorque = -7.0;  // Minimum allowable torque
-  float maxTorque = 7.0;  // Maximum allowable torque
-  float setpointTorque = 5.0;
-  float dynamicTorqueSensor;
-
-  //Time Variables
-  const long duration = 30; //in Sec
-  const long frecuency = 10; //in Hz
-  const long printingFrecuency = 100; //in ms
-  unsigned long currentMillis;
-  unsigned long previousMillisDuration;
-  unsigned long previousMillisFrecuency;
-  unsigned long previousMillisPrintingFrecuency;
-  bool flag = false;
-
-  // Return to Zero Mode
-  float posError;
-  float derivativeError;
-  float previousError = 0; // Store the previous error
-  float targetZero = 0.0;
-  float Kp = 15.0; // Proportional gain, adjust as needed
-  float Kd = 10.0; // Derivative gain, adjust as needed
-  float toleranceZero = 0.5; // Define a small tolerance for zero position
-  unsigned long currentZeroMillis;
-  unsigned long previousZeroMillis = 0;
-  const long intervalZero = 50; // Check every 50 milliseconds
-
   //eRob Instance
   eRob erob_1(eRob::eRob_90, CAN_ID, CS, INT_PIN, SPI);
   void (*interrupt_handlers[1])() = 
   {
       [](){ erob_1.handleInterrupt(); }
   };
-
-#endif 
-
-void getData()
-{
-  position = erob_1.getPosition();
-  degPosition = position* (180.0 / PI);
-  velocity = erob_1.getVelocity();
-  degVelocity = velocity* (180.0 / PI);
-  torque = erob_1.getTorque();
+ 
+void IRAM_ATTR myInterruptFunction() {
+  has_interrupted = true;
 }
 
-void control() //Control Function
-{
-  getData();
-
-  #if DynTest //Usar el dinamometro externo
-  dynamicTorqueSensor = dynSensor();
-  /*Start of Control*/
-  if (!erob_1.setTorque(setpointTorque, 2000))
+  void getData()  // Function to obtain the actual values of position, velocity and torque
   {
-    Serial.println("ERROR: SET TORQUE FAILED");
+    position = erob_1.getPosition();
+    degPosition = position* (180.0 / PI);
+    velocity = erob_1.getVelocity();
+    degVelocity = velocity* (180.0 / PI);
+    torque = erob_1.getTorque();
   }
-  /*End of Control*/
-  #endif
 
-  #if normalTest
-    /*Start of Control*/
-    if (!erob_1.setTorque(setpointTorque, 2000))
-    {
-      Serial.println("ERROR: SET TORQUE FAILED");
-    }
-    /*End of Control*/
-  #endif
-
-  #if rampTest
-    /*Start of Control*/
-    if (!erob_1.setTorque(setpointTorque, 2000))
-    {
-      Serial.println("ERROR: SET TORQUE FAILED");
-    }
-    /*End of Control*/
-    // Update the current ramp value based on the state
-    if (isRamping) {
-      runRamp();
-    }
-  #endif
-
-  #if positionTest
-    /*Start of Control*/
-    if (!erob_1.setTorque(setpointTorque, 2000))
-    {
-      Serial.println("ERROR: SET TORQUE FAILED");
-    }
-    /*End of Control*/
-  #endif
-}
-
-void goToZero() {  
-    unsigned long currentZeroMillis = millis();
-
+  void control()  // Control Function
+  {
     getData();
 
-    if (currentZeroMillis - previousZeroMillis >= intervalZero) {
-        previousZeroMillis = currentZeroMillis;
-        
-        // Get the current position
-        position = erob_1.getPosition();
-        degPosition = position * (180.0 / PI);
-        
-        // Check if we are within the tolerance range
-        if (abs(degPosition) > toleranceZero) {
-            // Calculate the error
-            posError = targetZero - position; // Target is 0, so error is -position
-
-            // Calculate the derivative of the error
-            derivativeError = (posError - previousError) / intervalZero;
-
-            // Calculate the torque using the PD controller
-            setpointTorque = Kp * posError + Kd * derivativeError;
-            
-            // Apply torque limits
-            if (setpointTorque > maxTorque) {
-                setpointTorque = maxTorque;
-            } else if (setpointTorque < minTorque) {
-                setpointTorque = minTorque;
-            }
-
-            // Apply torque to the motor
-            if (!erob_1.setTorque(setpointTorque, 2000)) {
-                Serial.println("ERROR: SET TORQUE FAILED");
-            }
-        }
+    #if DynTest //Usar el dinamometro externo
+    dynamicTorqueSensor = dynSensor();
+    /*Start of Control*/
+    if (!erob_1.setTorque(setpointTorque, 2000))
+    {
+      Serial.println("ERROR: SET TORQUE FAILED");
     }
-}
+    /*End of Control*/
+    #endif
 
-void printData()
-{
-  #if normalTest
-    //Serial.printf("Position= %f, Velocity= %f,  Torque= %f\n", position, velocity, torque);
-    Serial.printf("Setpoint= %f, Torque= %f, Position= %f, Velocidad= %f\n", setpointTorque, torque, degPosition, degVelocity);
-  #endif
+    #if normalTest
+      /*Start of Control*/
+      if (!erob_1.setTorque(setpointTorque, 2000))
+      {
+        Serial.println("ERROR: SET TORQUE FAILED");
+      }
+      /*End of Control*/
+    #endif
 
-  #if DynTest //Usar el dinamometro externo
-    Serial.printf("Setpoint= %f, Torque= %f, Torquimetro= %f\n", setpointTorque, torque, dynamicTorqueSensor);
-  #endif
+    #if buttonTest
+      if(has_interrupted)
+      {
+        Serial.println("Button pressed!");
+        has_interrupted = false;
+      }
+    #endif
 
-  #if rampTest
-    // Output the current ramp value
-    Serial.printf("Setpoint= %f, Torque= %f, Torquimetro= %f\n", setpointTorque, torque, dynamicTorqueSensor);
-  #endif  
+    #if rampTest
+      /*Start of Control*/
+      if (!erob_1.setTorque(setpointTorque, 2000))
+      {
+        Serial.println("ERROR: SET TORQUE FAILED");
+      }
+      /*End of Control*/
+      // Update the current ramp value based on the state
+      if (isRamping) {
+        runRamp();
+      }
+    #endif
 
-  #if positionTest
-    Serial.printf("Setpoint= %f, Torque= %f, Position= %f\n", setpointTorque, torque, degPosition);
-  #endif
-}
+    #if positionTest
+      /*Start of Control*/
+      if (!erob_1.setTorque(setpointTorque, 2000))
+      {
+        Serial.println("ERROR: SET TORQUE FAILED");
+      }
+      /*End of Control*/
+    #endif
+  }
 
-enum State // Definition of States of the menu
-{
-  STATE_IDLE,
-  STATE_WAITING_INPUT,
-  STATE_PROCESSING_INPUT
-};
+  void goToZero() // Function that returns the shaft to its HOME (0°)
+  {  
+      unsigned long currentZeroMillis = millis();
 
-enum OPTION : char //Definition of Finite-state machine (FSM) Options
-{
-  OPTION_SET_CONTROL_MODE = '0',
-  OPTION_TURN_ON = '1',
-  OPTION_TURN_OFF = '2',
-  OPTION_SET_CONTROL = '3',
-  OPTION_SET_TORQUE_0 = '4',
-  OPTION_HOME_POSITION = '5',
-  OPTION_MOTOR_RESPONSE = '6',
-  OPTION_CHANGE_SETPOINT = '7',  
-  OPTION_GO_ZERO = '8' 
-};
+      getData();
 
-State currentState = STATE_IDLE;
+      if (currentZeroMillis - previousZeroMillis >= intervalZero) {
+          previousZeroMillis = currentZeroMillis;
+          
+          // Get the current position
+          position = erob_1.getPosition();
+          degPosition = position * (180.0 / PI);
+          
+          // Check if we are within the tolerance range
+          if (abs(degPosition) > toleranceZero) {
+              // Calculate the error
+              posError = targetZero - position; // Target is 0, so error is -position
+
+              // Calculate the derivative of the error
+              derivativeError = (posError - previousError) / intervalZero;
+
+              // Calculate the torque using the PD controller
+              setpointTorque = Kp * posError + Kd * derivativeError;
+              
+              // Apply torque limits
+              if (setpointTorque > maxTorque) {
+                  setpointTorque = maxTorque;
+              } else if (setpointTorque < minTorque) {
+                  setpointTorque = minTorque;
+              }
+
+              // Apply torque to the motor
+              if (!erob_1.setTorque(setpointTorque, 2000)) {
+                  Serial.println("ERROR: SET TORQUE FAILED");
+              }
+          }
+      }
+  }
+
+  void printData()  // Function to print the data to the serial monitor depending on the testing mode
+  {
+    #if normalTest
+      //Serial.printf("Position= %f, Velocity= %f,  Torque= %f\n", position, velocity, torque);
+      Serial.printf("Setpoint= %f, Torque= %f, Position= %f, Velocidad= %f\n", setpointTorque, torque, degPosition, degVelocity);
+    #endif
+
+    #if DynTest //Usar el dinamometro externo
+      Serial.printf("Setpoint= %f, Torque= %f, Torquimetro= %f\n", setpointTorque, torque, dynamicTorqueSensor);
+    #endif
+
+    #if rampTest
+      // Output the current ramp value
+      // Serial.printf("Setpoint= %f, Torque= %f, Torquimetro= %f\n", setpointTorque, torque, dynamicTorqueSensor);
+      Serial.printf("Setpoint= %f, Torque= %f, Position= %f, Velocidad= %f\n", setpointTorque, torque, degPosition, degVelocity);
+    #endif  
+
+    #if positionTest
+      Serial.printf("Setpoint= %f, Torque= %f, Position= %f\n", setpointTorque, torque, degPosition);
+    #endif
+  }
+
+  enum State // Definition of States of the menu
+  {
+    STATE_IDLE,
+    STATE_WAITING_INPUT,
+    STATE_PROCESSING_INPUT
+  };
+
+  enum OPTION : char //Definition of Finite-state machine (FSM) Options
+  {
+    OPTION_SET_CONTROL_MODE = '0',
+    OPTION_TURN_ON = '1',
+    OPTION_TURN_OFF = '2',
+    OPTION_SET_CONTROL = '3',
+    OPTION_SET_TORQUE_0 = '4',
+    OPTION_HOME_POSITION = '5',
+    OPTION_MOTOR_RESPONSE = '6',
+    OPTION_CHANGE_SETPOINT = '7',  
+    OPTION_GO_ZERO = '8' 
+  };
+
+  State currentState = STATE_IDLE;
+
+#endif
 
 void setup()
 {
   Serial.begin(115200);
   SPI.begin();
+
+  // Configure the pin as an input with internal pull-up resistor
   pinMode(CTRL_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP); 
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), myInterruptFunction, FALLING); // Configure the interrupt
 
   #if DynTest //Usar el dinamometro externo
   // Communication with the Virtual Torque Sensor
@@ -316,7 +338,7 @@ void loop()
   switch (currentState)   // Handling the state machine
   {
     case STATE_IDLE:
-      Serial.println("***--- MENU INBIODROID ---***");
+      Serial.println("\n***--- MENU INBIODROID ---***");
       Serial.print("Set Control Mode: "); Serial.println(OPTION_SET_CONTROL_MODE - '0');
       Serial.print("Turn On: "); Serial.println(OPTION_TURN_ON - '0');
       Serial.print("Turn Off: "); Serial.println(OPTION_TURN_OFF - '0');
